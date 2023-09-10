43b4daee185f6c751757ed11e76990f414ce5418
szympajka <szymon.pajka@gmail.com>
2023-09-10 16:24:07 +0100

    test: debug

--- main.tsx
+++ main.tsx
@@ -7,16 +7,20 @@ const randomNumber = () => Math.round(Math.random() * 1000);
 const sleep = async (ms: number = 100) => await new Promise(resolve => setTimeout(resolve, ms));
 
 const asyncJob = async (server: Server) => {
+  console.log('Async job started');
   await sleep(randomNumber());
 
+  console.log('Async job message 1');
   server.publish("bridge", "Async Server Job message 1");
 
   await sleep(randomNumber());
 
+  console.log('Async job message 2');
   server.publish("bridge", "Async Server Job message 2");
 
   await sleep(randomNumber());
 
+  console.log('Async job message 3');
   server.publish("bridge", "Finish async job");
 }
 
